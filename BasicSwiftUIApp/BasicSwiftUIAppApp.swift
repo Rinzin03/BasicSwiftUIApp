0d0f02d4b607f17e334c236f9dbd15e5c732947b
Rinzin Lhamo <rinzin.l2024@gmail.com>
2026-09-04 20:41:18 +0600

    Initial Commit

---
+++ BasicSwiftUIApp/BasicSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSwiftUIAppApp.swift
+//  BasicSwiftUIApp
+//
+//  Created by iMac01 on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct BasicSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
